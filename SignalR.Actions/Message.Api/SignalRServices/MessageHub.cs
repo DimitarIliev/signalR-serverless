c29bfecf471e3c11eb3946c621d1756cfbfd16d1
Dimitar Iliev <diki_tazo@hotmail.com>
2023-05-26 00:39:42 +0200

    Add comment to code.

--- SignalR.Actions/Message.Api/SignalRServices/MessageHub.cs
+++ SignalR.Actions/Message.Api/SignalRServices/MessageHub.cs
@@ -4,6 +4,7 @@ namespace Message.Api.SignalRServices
 {
     public class MessageHub : Hub
     {
+        // Invoke from Clients to send messages
         public async Task SendMessage(string message)
         {
             await Clients.All.SendAsync("broadcast", message);
